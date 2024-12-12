1c1597c269d54110f9c54914e46c8e94b0a802d6
seonayoung <hawwah03@gachon.ac.kr>
2024-12-12 13:58:16 +0900

    fix: SignUp_Profile 페이지 css 수정 / 닉네임 에러메시지 추가
    - Button 재정렬 및 재배치
    - 닉네임 text-input 크기 조정
    - user 아이콘 삭제
    - 닉네임 입력값 검증, 에러메시지 코드 추가

--- src/components/Web_SignUp_Profile/User.jsx
+++ src/components/Web_SignUp_Profile/User.jsx
@@ -1,11 +1,5 @@
-/*
-We're constantly improving the code you see. 
-Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
-*/
-
 import PropTypes from "prop-types";
 import React from "react";
-import UserIcon from "./UserIcon";
 import "./style.css";
 
 export const User = ({ style }) => {
@@ -22,8 +16,6 @@ export const User = ({ style }) => {
           }
         />
       )}
-
-      {style === "solid" && <UserIcon className="user" />}
     </>
   );
 };
