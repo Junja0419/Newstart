e4ffceeb85761ab90f7f143ba312e652940ac4cb
seonayoung <hawwah03@gachon.ac.kr>
2024-12-14 22:08:14 +0900

    feat: 프로필 조회/편집/비밀번호 재설정 페이지 추가
    - 프로필 조회 페이지 (알림 on/off 기능 미구현) / 프로필 편집 페이지 / 비밀번호 재설정 페이지 기능 추가 완료
    - 비밀번호 재설정 페이지 경로명 App.js에서 수정

--- src/components/Web_Profile_Password/ChangeButton.jsx
+++ src/components/Web_Profile_Password/ChangeButton.jsx
@@ -1,15 +1,20 @@
-/*
-We're constantly improving the code you see. 
-Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
-*/
-
 import PropTypes from "prop-types";
 import React from "react";
 import "./style.css";
 
-export const ChangeButton = ({ className, divClassName, text = "완료" }) => {
+export const ChangeButton = ({
+  className,
+  divClassName,
+  text,
+  disabled,
+  onClick,
+}) => {
   return (
-    <div className={`wpp-div-wrapper ${className}`}>
+    <div
+      className={`wpp-div-wrapper disabled-${disabled} ${className}`}
+      onClick={onClick}
+      style={{ cursor: "pointer" }}
+    >
       <div className={`text-wrapper-2 ${divClassName}`}>{text}</div>
     </div>
   );
@@ -17,6 +22,8 @@ export const ChangeButton = ({ className, divClassName, text = "완료" }) => {
 
 ChangeButton.propTypes = {
   text: PropTypes.string,
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
 };
 
 export default ChangeButton;
