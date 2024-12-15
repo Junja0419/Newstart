5407dc6592b38f030864c8cf4473a6e66bb5ec6f
junja <ajuni0419@gmail.com>
2024-12-15 23:09:29 +0900

    update SecurityConfig.java, AuthControlle.java

--- backend/src/main/java/com/project/newstart/config/SecurityConfig.java
+++ backend/src/main/java/com/project/newstart/config/SecurityConfig.java
@@ -88,7 +88,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests((auth) -> auth
                         .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
                         .requestMatchers("/login/**", "/oauth2/**", "/auth/**").permitAll() //login, /, join 경로 요청이 오면 모든 권한 허용
-                        .requestMatchers("/css/**", "images/**", "/js/**", "/login/*", "/logout/*", "/posts/**", "/comments/**", "/error").permitAll()
+                        .requestMatchers("/css/**", "images/**", "/js/**", "/login/**", "/logout/*", "/posts/**", "/comments/**", "/error").permitAll()
                         .anyRequest().authenticated()); //위 경로 외의 요청이 오면 로그인 후 이용 가능
 
         //OAuth2.0
@@ -101,7 +101,7 @@ public class SecurityConfig {
         //form 로그인 방식
         http
                 .formLogin((auth) -> auth
-                        .loginPage("/auth/email/login")
+                        .loginPage("https://siyeon-3faf5.web.app/auth/email/login")
                         .loginProcessingUrl("/auth/email/loginProcess")
                         .defaultSuccessUrl("/")
                         .permitAll());
@@ -116,7 +116,7 @@ public class SecurityConfig {
         //세션 설정
         http
                 .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).sessionFixation((sessionFixation) -> sessionFixation.newSession())
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED).sessionFixation((sessionFixation)->sessionFixation.newSession())
                         .maximumSessions(1)
                         .maxSessionsPreventsLogin(true));
 
