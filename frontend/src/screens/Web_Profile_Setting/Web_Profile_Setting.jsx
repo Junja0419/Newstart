b10d47c7e3ddb04c5ea0448fc0bf05e1bab4e487
seonayoung <hawwah03@gachon.ac.kr>
2024-12-17 22:01:16 +0900

    fix: useEffect import 해오도록 수정

--- frontend/src/screens/Web_Profile_Setting/Web_Profile_Setting.jsx
+++ frontend/src/screens/Web_Profile_Setting/Web_Profile_Setting.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useWindowWidth } from "../../breakpoints";
 import Return from "../../components/Web_Profile_Setting/Return";
