cffbdfb49bb0c39179cd6ac12679fc9c6e3b4a8f
junja <ajuni0419@gmail.com>
2024-12-17 15:42:39 +0900

    edit MainController.java

--- backend/src/main/java/com/project/newstart/controller/MainController.java
+++ backend/src/main/java/com/project/newstart/controller/MainController.java
@@ -5,20 +5,18 @@ import com.project.newstart.entity.Headline;
 import com.project.newstart.entity.Summary;
 import com.project.newstart.entity.UserEntity;
 import com.project.newstart.repository.UserRepository;
-import com.project.newstart.service.CallService;
 import com.project.newstart.service.HeadlineService;
 import com.project.newstart.service.SummaryService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import java.text.ParseException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 
 @RestController
 public class MainController {
@@ -26,7 +24,6 @@ public class MainController {
     private final HeadlineService headlineService;
     private final SummaryService summaryService;
     private final UserRepository userRepository;
-
 
     public MainController(HeadlineService headlineService, SummaryService summaryService, UserRepository userRepository) {
         this.headlineService = headlineService;
