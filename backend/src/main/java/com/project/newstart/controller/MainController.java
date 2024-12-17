1716adf4046931118de60b8fe93cea607e233cb8
junja <ajuni0419@gmail.com>
2024-12-18 05:40:31 +0900

    edit MainController.java

--- backend/src/main/java/com/project/newstart/controller/MainController.java
+++ backend/src/main/java/com/project/newstart/controller/MainController.java
@@ -10,6 +10,7 @@ import com.project.newstart.service.SummaryService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
